7d428f51a5c278d5c501f504d7d0c6a6acd90cb9
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2024-03-22 02:29:23 +0000

    他のユーザーからのアクセスを制限しよう_20240322 #5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
@@ -31,3 +33,12 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :profile_image)
   end
 end
+
+ # ここから追加
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
+  # ここまで追加
